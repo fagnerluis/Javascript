931514ad1fed1dbde9f90cf034564f15176342de
Luis Fagner <luis_marques18@outlook.com>
2022-11-30 12:22:39 -0400

    Array-node

---
+++ array/media.js
@@ -0,0 +1,10 @@
+//const nota1 =10;
+//const nota2 = 6.5;
+//const nota3 = 8 ;
+
+//array
+
+const notas = [ 10,6.5,7.5];
+
+const media =  (notas[0]+ notas[1] + notas[2]  )/ notas.length ; // todo o tamanho da media 
+console.log(media);
